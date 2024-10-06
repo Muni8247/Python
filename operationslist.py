e740b1eb88f098286761cef002a96f7892dbaf82
Katigari Naga Muni Reddy <100259996+Muni8247@users.noreply.github.com>
2024-10-06 18:14:47 +0530

    Add files via upload

---
+++ operationslist.py
@@ -0,0 +1,27 @@
+#Arithmatic
+l=[1,2,3,4,5]
+l1=[6,7,8,9,10]
+print(l)
+print(l1)
+
+#Arithmatic operations use only (+ , *)
+#Concatenate//Merge
+l2=l+l1
+print(l2)
+
+#Multiplication
+print("***************Multiplication*****************")
+print(l*3)
+print(l1*3)
+
+#Membership Operators
+l=[1,2,3,4,5]
+l1=[6,7,8,[9,10]]
+print(l)
+print(l1)
+
+print(5 in l)
+print(10 in l1)
+print([10] in l1)
+print([9,10] in l1)
+
